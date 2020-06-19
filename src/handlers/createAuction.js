4a517518c9bf1e4aa5dccb4c6a072a740e217fb1
Sumit Govil <sumit.govil@optus.com.au>
2020-06-19 15:14:38 +1000

    simple auction service and some optimisation

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,27 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+  const auction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+  };
+  await dynamodb
+    .put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    })
+    .promise();
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = createAuction;
