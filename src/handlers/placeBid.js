b980c6fd0b0f1bf9557e424b3aa2fbdfdd40b207
Sumit Govil <sumit.govil@optus.com.au>
2020-06-19 17:16:29 +1000

    bid placement

---
+++ src/handlers/placeBid.js
@@ -0,0 +1,41 @@
+import AWS from "aws-sdk";
+import createError from "http-errors";
+import commonMiddleware from "../lib/commonMiddleware";
+import { getAuctionById } from "./getAuction";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+  let updatedAuction;
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+  const auction = await getAuctionById(id);
+
+  if (amount <= auction.highestBid.amount) {
+    throw new createError.Forbidden("Current bid should be higher");
+  }
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id },
+    UpdateExpression: "set highestBid.amount = :amount",
+    ExpressionAttributeValues: {
+      ":amount": amount,
+    },
+    ReturnValues: "ALL_NEW",
+  };
+
+  try {
+    const result = await dynamodb.update(params).promise();
+    updatedAuction = result.Attributes;
+  } catch (error) {
+    console.log("error", error);
+    throw new createError.InternalServerError(error);
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
+}
+
+export const handler = commonMiddleware(placeBid);
